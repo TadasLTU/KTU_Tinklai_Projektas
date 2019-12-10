c2fe77762b3939763f25ca7f49cc75db6a12f144
TadasLTU <semionovas13@gmail.com>
2019-12-11 00:17:58 +0200

    Initial comit

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+class config {
+	const DB_SERVER    = 'localhost';
+	const DB_NAME      = 'tadsem';
+	const DB_USERNAME  = 'tadsem';
+	const DB_PASSWORD  = 'aigiek7aiK9BaeXo';
+	const DB_PREFIX	   = '';
+	const NUMBER_OF_ROWS_IN_PAGE = 10;
+}
+
+?>
